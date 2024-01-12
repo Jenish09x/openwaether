31f0fc7d1d2225d42483980335b2bc3c48148fae
Jeniss09 <jenishmevada1@gmail.com>
2024-01-12 17:24:13 +0530

    theme change

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ void main() {
       ],
       child: Consumer<ThemeProvider>(
         builder: (BuildContext context, value, Widget? child) {
+          value.changeTheme();
           return MaterialApp(
             theme: value.isLight?darkTheme:lightTheme,
             debugShowCheckedModeBanner: false,
